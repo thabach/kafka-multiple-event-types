4ea499bba11003156b59b332f368786a853b4aa4
thabach <cbach@computer.org>
2025-04-21 11:10:00 +0200

    use logger

--- kafka-multiple-event-types_client/listener_client/src/main/java/io/doubledispatch/kafka/listener_client/event_handling/PageEventHandler.java
+++ kafka-multiple-event-types_client/listener_client/src/main/java/io/doubledispatch/kafka/listener_client/event_handling/PageEventHandler.java
@@ -1,6 +1,8 @@
 package io.doubledispatch.kafka.listener_client.event_handling;
 
 import org.apache.avro.specific.SpecificRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.lmax.disruptor.EventHandler;
 
@@ -9,15 +11,17 @@ import io.doubledispatch.kafka.multiple_event_types.avro.Purchase;
 
 public class PageEventHandler implements EventHandler<PageEventHolder> {
 
+    private final static Logger logger = LoggerFactory.getLogger(PageEventHandler.class);
+
     @Override
     public void onEvent(PageEventHolder event, long sequence, boolean endOfBatch) throws Exception {
         SpecificRecord record = event.getPageEvent();
         if (record instanceof PageView) {
-            System.out.printf("[Avro] Found a PageView event %s %n", record);
+            logger.info("[Avro] Found a PageView event %s %n", record);
         } else if (record instanceof Purchase) {
-            System.out.printf("[Avro] Found a Purchase event %s %n", record);
+            logger.info("[Avro] Found a Purchase event %s %n", record);
         } else {
-            System.out.printf(String.format("Unrecognized type %s %n", record.getSchema().getFullName()));
+            logger.info(String.format("Unrecognized type %s %n", record.getSchema().getFullName()));
         }
         Thread.sleep(30_000);
         event.getAcknowledgment().acknowledge();; 
